3b9f15594433b89cce2ea33ad13f7c6fbede595e
Yardie83 <hermann.grieder@gmail.com>
2016-09-01 15:04:07 +0200

    added tiles

--- src/ch/atlantis/game/GameBoard.java
+++ src/ch/atlantis/game/GameBoard.java
@@ -2,7 +2,6 @@ package ch.atlantis.game;
 
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 
 import java.util.Random;
@@ -20,37 +19,42 @@ public class GameBoard extends Pane {
         int side = widthValue / (widthValue / (heightValue / gridCount));
         int columnCount = widthValue / side;
         Random rand = new Random();
+        tiles = new Tile[columnCount][gridCount];
 
-        for (int x = 0;x < columnCount; x++) {
+        for (int x = 0; x < columnCount; x++) {
             for (int y = 0; y < gridCount; y++) {
                 tiles[x][y] = new Tile(x * side, y * side, side, Tiletype.EMPTY);
+                System.out.println("X: " + x + " " + "Y: " + y);
                 Rectangle rect = new Rectangle();
                 rect.setWidth(side);
                 rect.setHeight(side);
-                rect.setFill(Color.rgb(rand.nextFloat(), ));
+                rect.setFill(Color.rgb((int) (rand.nextFloat() * 255), (int) (rand.nextFloat() * 255), (int) (rand.nextFloat() * 255)));
+                rect.setX(x * side);
+                rect.setY(y * side);
+                this.getChildren().add(rect);
             }
         }
 
-        for (int i = 0; i < gridCount; i++) {
-            Line horizontalLine = new Line();
-            horizontalLine.setStartX(0);
-            horizontalLine.setEndX(widthValue);
-            horizontalLine.setStartY(i * side);
-            horizontalLine.setEndY(i * side);
-            horizontalLine.setStroke(Color.BLACK);
-
-            this.getChildren().addAll(horizontalLine);
-        }
-
-        for (int i = 0; i * side < widthValue; i++) {
-            Line verticalLine = new Line();
-            verticalLine.setStartY(0);
-            verticalLine.setEndY(heightValue);
-            verticalLine.setStartX(i * side);
-            verticalLine.setEndX(i * side);
-            verticalLine.setStroke(Color.BLACK);
-
-            this.getChildren().addAll(verticalLine);
-        }
+//        for (int i = 0; i < gridCount; i++) {
+//            Line horizontalLine = new Line();
+//            horizontalLine.setStartX(0);
+//            horizontalLine.setEndX(widthValue);
+//            horizontalLine.setStartY(i * side);
+//            horizontalLine.setEndY(i * side);
+//            horizontalLine.setStroke(Color.BLACK);
+//
+//            this.getChildren().addAll(horizontalLine);
+//        }
+//
+//        for (int i = 0; i * side < widthValue; i++) {
+//            Line verticalLine = new Line();
+//            verticalLine.setStartY(0);
+//            verticalLine.setEndY(heightValue);
+//            verticalLine.setStartX(i * side);
+//            verticalLine.setEndX(i * side);
+//            verticalLine.setStroke(Color.BLACK);
+//
+//            this.getChildren().addAll(verticalLine);
+//        }
     }
 }
